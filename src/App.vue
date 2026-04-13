64e8f65f602cf9f29fd2738712f7e90811f7267a
ArifGameka <silrein4869@gmail.com>
2026-04-13 14:53:16 +0800

    Updated website's version

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 
     <div class="fixed bottom-4 left-4 z-50 pointer-events-none select-none">
       <span class="text-[10px] font-mono font-bold tracking-widest text-gray-400/60 dark:text-gray-600/50 uppercase">
-        Enlightened Wiki v1.1.0-P2-silrein
+        Enlightened Wiki v1.1.1-P2-silrein
       </span>
     </div>
 
